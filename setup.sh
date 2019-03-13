9f1437a74126b4dccd00c5832d9ffcd533a62bfd
sammyas <sammyas@pacbell.net>
2019-03-12 17:28:49 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,3 +9,6 @@ sudo apt-get install ctags
 go get -u github.com/jstemmer/gotags #for go!
 cd ~/.vim/bundle
 git clone https://github.com/majutsushi/tagbar.git
+git clone https://github.com/tpope/vim-fugitive.git
+vim -u NONE -c "helptags vim-fugitive/doc" -c q
+echo "Now Follow the directions to set up ctags git hooks from https://tbaggery.com/2011/08/08/effortless-ctags-with-git.html"
